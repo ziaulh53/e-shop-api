fa7e8acb165fcfec8792eade288e18620d875767
ziaulh53 <ziaul.siu@gmail.com>
2023-08-29 23:48:05 +0600

    sdf

--- src/controllers/admin/order.js
+++ src/controllers/admin/order.js
@@ -1,39 +1,88 @@
 import { v4 } from "uuid";
 import { OrderModel } from "../../models";
-import uniqid from 'uniqid';
+import uniqid from "uniqid";
+import { ISO_8601 } from "moment/moment";
 
 const SSK =
   "sk_test_51NiaBnG5LWC441mfG5iQbJADmdDYM3XncuwwhYoHmk1B4vuLCqN5Qgi5qTlp6o9ia5UEA8nnxNsw6eWWOBp00TKB00CLr736Ij";
 const stripe = require("stripe")(SSK);
 // console.log(process.env.STRIPE_SECRET_KEY, 'xc')
 
-
 //get orders
-export const getOrdersAdmin = async (req,res)=>{
-  try {
-    const result = await OrderModel.find().populate('user');
-    return res.status(200).json({success: true, result})
-  } catch (error) {
-    console.log(error)
+export const getOrdersAdmin = async (req, res) => {
+  const { startDate, endDate, orderId, status, email, phone } = req.query;
+  let filter = {};
+  if (startDate) {
+    filter = {
+      "cratedAt": { $gte: new Date(startDate), $lte: new Date(endDate) },
+    };
   }
-}
-export const getSingleOrderAdmin = async (req,res)=>{
-  const id = req.params.id;
+  if (orderId){
+    filter = {
+      ...filter, "orderId": orderId
+    }
+  };
+  if (status){
+    filter = {
+      ...filter, "status": status
+    }
+  };
+  if (email){
+    filter = {
+      ...filter, "user.email": email
+    }
+  };
+  if (phone){
+    filter = {
+      ...filter, "shippingAddress.phone": phone
+    }
+  };
   try {
-    const result = await OrderModel.findById(id).populate('user');
-    return res.status(200).json({success: true, result})
-  } catch (error) {
-    console.log(error)
-  }
-}
+    const pripeline = [
+      {
+        $lookup: {
+          from: "users",
+          localField: "user",
+          foreignField: "_id",
+          as: "user",
+        },
+      },
+      {
+        $unwind: "$user",
+      },
+      {
+        $match: {
+          ...filter
+        },
+      },
+    ];
 
-export const updateStatusAdmin = async (req,res)=>{
-  const id = req.params.id;
-  const {status} = req.body;
-  try {
-    await OrderModel.findByIdAndUpdate(id, {status});
-    return res.status(200).json({success: true, msg: status==='Cancelled'?"Order Cancelled":"Order status updated"})
+    const result = await OrderModel.aggregate(pripeline);
+    return res.status(200).json({ success: true, result });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
+export const getSingleOrderAdmin = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await OrderModel.findById(id).populate("user");
+    return res.status(200).json({ success: true, result });
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const updateStatusAdmin = async (req, res) => {
+  const id = req.params.id;
+  const { status } = req.body;
+  try {
+    await OrderModel.findByIdAndUpdate(id, { status });
+    return res.status(200).json({
+      success: true,
+      msg: status === "Cancelled" ? "Order Cancelled" : "Order status updated",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
